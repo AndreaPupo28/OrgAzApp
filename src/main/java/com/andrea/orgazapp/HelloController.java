0450722139e52df6033eeaadef20e35a8b13f92b
Andrea Pupo <andreapupo2002@gmail.com>
2024-12-05 12:34:28 +0100

    Inizializzazione del progetto

---
+++ src/main/java/com/andrea/orgazapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.andrea.orgazapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
